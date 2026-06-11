1982343d3d43ad72334c93c1e665f01a8b5105e5
Tomás Godoy <tomas.godoy.litio@hotmail.com>
2026-06-11 17:11:58 -0300

    Actuañlizacion de errores

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 // ----------------------------- Funciones -----------------------------
@@ -21,11 +22,11 @@ int menuGeneral()
 }
 
 // Listado Alumnos
-int listadoAlumnos(int alumnos[5][30], int cantidad)
+int listadoAlumnos(char alumnos[][6][30], int cantidad)
 {
     for (int i = 0; i < cantidad; i++)
     {
-        printf("%s | %s | %s | %s | %s \n", alumnos[i][0], alumnos[i][0], alumnos[i][0], alumnos[i][0], alumnos[i][0]);
+        printf("%s | %s | %s | %s | %s \n", alumnos[i][0], alumnos[i][1], alumnos[i][2], alumnos[i][3], alumnos[i][4]);
     }
 }
 // ----------------------------- Main -----------------------------
@@ -33,7 +34,7 @@ int listadoAlumnos(int alumnos[5][30], int cantidad)
 int main()
 {
     // VARIABLES Y ARRAYS
-    int cantidadDeAlumnos;
+    int cantidadDeAlumnos = 0;
     int opcionvalidada;
     // Tipo int datos alumno
     int cantidadnumerosendni;
@@ -49,7 +50,9 @@ int main()
     int cantidadocupadateatro;
     int cantidadocupadafotografia;
     // Arrays
-    char alumno[5][30];
+    // Almacenamiento de alumnos
+    char alumnos[100][6][30];
+    // Talleres
     char disciplinas[4][4][15] = {
         {"PNT", "Pintura", "25000", "0"},
         {"ESC", "Escultura", "14000", "0"},
@@ -57,86 +60,82 @@ int main()
         {"FOT", "Fotografía", "23000", "0"},
     };
 
-    // Menú y validación
-    opcionvalidada = menuGeneral();
-    while (opcionvalidada < 1 || opcionvalidada > 7)
+    do
     {
-        printf("Opción inválida. Intente nuevamente.\n\n");
+        // Menú y validación
         opcionvalidada = menuGeneral();
-    }
-
-    switch (opcionvalidada)
-    {
-    case 1:
-        // Cantidad de alumnos y validación
-        printf("Cantidad de alumnos que quiere inscribir: ");
-        scanf("%d", &cantidadDeAlumnos);
-
-        // Almacenamiento de alumnos
-        char alumnos[cantidadDeAlumnos][5][30];
-
-        while (cantidadDeAlumnos < 1)
+        while (opcionvalidada < 1 || opcionvalidada > 7)
         {
-            printf("Dato inváido. Ingrese que cantidad de alumnos desea inscribir: ");
-            scanf("%d", &cantidadDeAlumnos);
+            printf("Opción inválida. Intente nuevamente.\n\n");
+            opcionvalidada = menuGeneral();
         }
 
-        // Pedir datos del alumno, validarlos y almacenarlos
-        for (int i = 0; i < cantidadDeAlumnos; i++)
+        if (opcionvalidada == 1)
         {
-            printf("Ingrese los datos del alumno n° %d \n", i + 1);
-
-            // DNI
-            printf("Dni: ");
-            scanf("%s", &alumno[0][0]);
-            // Pasarlo a int
-            dninumerico = strtol(&alumno[0][0], NULL, 10);
-            // Contador del dni
-            cantidadnumerosendni = strlen(&alumno[0][0]);
-
-            // Validación del DNI
-            while (cantidadnumerosendni < 6 || cantidadnumerosendni > 9 || dninumerico < 10000000 || dninumerico > 60000000)
+            // Cantidad de alumnos y validación
+            printf("Cantidad de alumnos que quiere inscribir: ");
+            scanf("%i", &cantidadDeAlumnos);
+            while (cantidadDeAlumnos < 1)
             {
-                printf("El dni es inválido, recuerda que debe ser mayor a 10 millones y menor que 60 millones.\n");
-                scanf("%s", &alumno[0][0]);
-                // Pasarlo a int
-                dninumerico = strtol(&alumno[0][0], NULL, 10);
-                // Contador del dni
-                cantidadnumerosendni = strlen(&alumno[0][0]);
+                printf("Dato inváido. Ingrese que cantidad de alumnos desea inscribir: ");
+                scanf("%d", &cantidadDeAlumnos);
             }
 
-            // NOMBRE APELLIDO Y EDAD
-            printf("Apellido: ");
-            scanf("%s", &alumno[1][0]);
-            printf("Nombre: ");
-            scanf("%s", &alumno[2][0]);
-            printf("Edad: ");
-            scanf("%s", &alumno[3][0]);
-            edadnumerico = strtol(&alumno[3][0], NULL, 10);
+            // Pedir datos del alumno, validarlos y almacenarlos
+            for (int i = 0; i < cantidadDeAlumnos; i++)
+            {
+                printf("Ingrese los datos del alumno n° %d \n", i + 1);
 
-            // CÓDIGO DE TALLER
-            printf("Código del taller: ");
-            printf("Recuerda que los códigos són(en mayúsculas): \n ");
-            printf("PNT para Pintura. \n ");
-            printf("ESC para Escultura. \n ");
-            printf("THR para Teatro. \n ");
-            printf("FOT para Fotografía. \n ");
-            scanf("%s", &alumno[4][0]);
+                // DNI
+                printf("Dni: ");
+                scanf("%s", alumnos[i][0]);
+                // Pasarlo a int
+                dninumerico = strtol(alumnos[i][0], NULL, 10);
+                // Contador del dni
+                cantidadnumerosendni = strlen(alumnos[i][0]);
 
-            // MEDIO DE PAGO
-            printf("Medio de pago: ");
-            printf("1 - Efectivo");
-            printf("2 - Crédito");
-            scanf("%s", &alumno[5][0]);
+                // Validación del DNI
+                while (cantidadnumerosendni < 6 || cantidadnumerosendni > 9 || dninumerico < 10000000 || dninumerico > 60000000)
+                {
+                    printf("El dni es inválido, recuerda que debe ser mayor a 10 millones y menor que 60 millones.\n");
+                    scanf("%s", alumnos[i][0]);
+                    // Pasarlo a int
+                    dninumerico = strtol(alumnos[i][0], NULL, 10);
+                    // Contador del dni
+                    cantidadnumerosendni = strlen(alumnos[i][0]);
+                }
+
+                // NOMBRE APELLIDO Y EDAD
+                printf("Apellido: ");
+                scanf("%s", alumnos[i][1]);
+                printf("Nombre: ");
+                scanf("%s", alumnos[i][2]);
+                printf("Edad: ");
+                scanf("%s", alumnos[i][3]);
+                edadnumerico = strtol(alumnos[i][3], NULL, 10);
+
+                // CÓDIGO DE TALLER
+                printf("Código del taller: ");
+                printf("Recuerda que los códigos són(en mayúsculas): \n ");
+                printf("PNT para Pintura. \n ");
+                printf("ESC para Escultura. \n ");
+                printf("THR para Teatro. \n ");
+                printf("FOT para Fotografía. \n ");
+                scanf("%s", alumnos[i][4]);
+
+                // MEDIO DE PAGO
+                printf("Medio de pago: ");
+                printf("1 - Efectivo");
+                printf("2 - Crédito");
+                scanf("%s", alumnos[i][5]);
+            }
+        }
+        else if (opcionvalidada == 2)
+        {
+            listadoAlumnos(alumnos, cantidadDeAlumnos);
 
             break;
         }
-
-    case 2:
-        listadoAlumnos(&alumno, cantidadDeAlumnos);
-
-        break;
-    }
-
+    } while (opcionvalidada != 7);
     return 0;
 }
